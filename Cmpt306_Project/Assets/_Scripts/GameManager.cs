e362670d72e3d73ea0a2772aea645751d1d3038a
Aifaz Karedia <karedia.aifaz23@gmail.com>
2022-11-22 16:54:33 -0600

    Boss Arrival Canvas

--- Cmpt306_Project/Assets/_Scripts/GameManager.cs
+++ Cmpt306_Project/Assets/_Scripts/GameManager.cs
@@ -11,15 +11,18 @@ public class GameManager : MonoBehaviour
     public static GameManager instance = null; 
     public GameObject player; 
     public GameObject camera;
-    private static bool isBossPhase = false;
+    public static bool isBossPhase = false;
     public static int score = 0;
+    public int counter  = 0;
     public float scoreRate = 1f;
     private float nextScore = 0.0f;
     public TextMeshProUGUI scoreTxt;
     public bool playerDead = false;
     public TextMeshProUGUI text;
+    public TextMeshProUGUI arriveText;
     public EnemySpawner enemySpawner;
     public GameObject gameOver;
+    public GameObject bossCanvas;
     public GameObject pauseMenu;
     public static bool gameEnded = false;
 
@@ -27,6 +30,7 @@ public class GameManager : MonoBehaviour
         gameEnded = false;
         isBossPhase = false;
         gameOver.SetActive(false);
+        bossCanvas.SetActive(false);
         pauseMenu.SetActive(true);
         playerDead = false;
         score = 0;
@@ -36,8 +40,16 @@ public class GameManager : MonoBehaviour
         if (Time.time > nextScore && isBossPhase == false && playerDead == false){
             nextScore = Time.time + scoreRate;
             score += 1;
+            counter += 1;
             print(score);
+            if(counter == 17) {
+                bossCanvas.SetActive(true);
+                ArrivalText script = arriveText.GetComponent<ArrivalText>();
+                script.blink();
+            }
+
             if(score%20 == 0){
+                counter = 0;
                 changePhase();
             }
         }
