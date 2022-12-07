32a6806063d522ee1388fa413cd611c6918a0c54
LiamHilkewich <liamhilkewich@gmail.com>
2022-12-07 13:51:25 -0600

    minor changes

--- Cmpt306_Project/Assets/_Scripts/EnemySpawner.cs
+++ Cmpt306_Project/Assets/_Scripts/EnemySpawner.cs
@@ -97,7 +97,7 @@ public class EnemySpawner : MonoBehaviour
 
     public void SpawnBoss(){
         Vector3 cameraPosition = GameManager.instance.camera.transform.position;
-        Vector3 bossLocation = new Vector3(cameraPosition.x,1.32f,cameraPosition.z+10);
+        Vector3 bossLocation = new Vector3(cameraPosition.x,1.32f,cameraPosition.z+5);
         Instantiate(bossPrefab, bossLocation, transform.rotation);
     }
 }
