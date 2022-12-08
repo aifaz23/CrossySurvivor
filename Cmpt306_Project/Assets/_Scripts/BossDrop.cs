c5613bdd180d9867ae5f5da80ca2de41161f3c38
LiamHilkewich <liamhilkewich@gmail.com>
2022-12-07 18:34:24 -0600

    game is paused when boss drop appears

--- Cmpt306_Project/Assets/_Scripts/BossDrop.cs
+++ Cmpt306_Project/Assets/_Scripts/BossDrop.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class BossDrop : MonoBehaviour
 {
     public GameObject bossMenuUI;
+
+    void Start(){
+        FindObjectOfType<AudioManager>().changeVolume("BackgroundMusic", 0.035f);
+        Time.timeScale = 0f;
+    }
     
     public void increaseHP(){
         GameObject.Find("Player").GetComponent<Player>().maxHealth+=25;
         GameObject.Find("Player").GetComponent<Player>().health+=25;
         Destroy(this.bossMenuUI); 
         GameManager.instance.changePhase(); // Impliment going to next stage here
+        FindObjectOfType<AudioManager>().changeVolume("BackgroundMusic", 0f);
+        Time.timeScale = 1f;
         
     }
     public void revieveNewWeapon(){
         GameObject.Find("GunSwitcher").GetComponent<GunSwitching>().recieveGun();
         Destroy(this.bossMenuUI);
         GameManager.instance.changePhase(); // Impliment going to next stage here
-   
+        FindObjectOfType<AudioManager>().changeVolume("BackgroundMusic", 0f);
+        Time.timeScale = 1f;
     }
    
 }
